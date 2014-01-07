a11cc9384e8e69b7115d4c59f58dec120cc85a5f
Prescott Murphy <prurph@gmail.com>
2014-01-07 11:45:53 -0500

    Created basic_ruby.rb and implemented puts statements

---
+++ basic_ruby.rb
@@ -0,0 +1,26 @@
+my_child_age = 2
+puts "my child's age is #{my_child_age}" # added some puts statements
+
+my_age = 31
+puts "my age is #{my_age}"
+
+current_year = 2014
+puts "current year is #{current_year}"
+
+child_dob = current_year - my_child_age
+puts "child's date of birth = #{child_dob}"
+puts "child's date of birth = #{current_year - my_child_age}"
+
+
+my_dob = current_year - my_age
+puts "my date of birth = #{my_dob}"
+
+my_dads_age = 61
+my_dads_dob = current_year - my_dads_age
+
+my_first_name = "Prescott"
+
+my_last_name = "Murphy"
+
+my_full_name = my_first_name + " " + my_last_name
+puts "my full name is #{my_full_name}"
